2a7ba0a7727ece27287c0d3ab7c7875074df9493
stepin104353 <69048783+stepin104353@users.noreply.github.com>
2020-09-28 01:17:49 +0530

    Add files via upload

---
+++ test/test.c
@@ -0,0 +1,79 @@
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+
+/* Modify these two lines according to the project */
+#include <calculator_operations.h>
+#define PROJECT_NAME    "Calculator"
+
+/* Prototypes for all the test functions */
+int test_add(int number1,int number2);
+int test_subtract(int number1,int number2);
+int test_multiply(int number1,int number2);
+int test_divide(int number1,int number2);
+int test_modulus(int number1,int number2);
+
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+  
+  /* Add your test functions in this format for testing*/
+  CU_add_test(suite, "add", test_add);
+  CU_add_test(suite, "subtract", test_subtract);
+  CU_add_test(suite, "multiply", test_multiply);
+  CU_add_test(suite, "divide", test_divide);
+  CU_add_test(suite, "modulus", test_modulus);
+  
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
+
+/* Write all the test functions */
+int test_add(int number1,int number2) {
+  CU_ASSERT(30 == add(10, 20));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1500 == add(750, 7500));
+}
+
+int test_subtract(int number1,int number2) {
+  CU_ASSERT(-3 == subtract(0, 3));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == subtract(1000, 900));
+}
+
+int test_multiply(int number1,int number2) {
+  CU_ASSERT(0 == multiply(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(2 == multiply(2, 5));
+}
+
+int test_divide(int number1,int number2) {
+  CU_ASSERT(0 == divide(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == divide(2, 2));
+}
+int test_modulus(int number1,int number2) {
+  CU_ASSERT(0 == modulus(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == modulus(2, 2));
+}
